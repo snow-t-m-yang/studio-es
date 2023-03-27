7789a38860f79d669fc51c1a8c3813a1ef4e701c
Young <ytm199891@gmail.com>
2023-03-27 10:00:43 +0800

    Layout created, sidebar Icons added, Building Navb ar.
    Changes to be committed:
    	new file:   compoents/Navbar.tsx
    
    Changes not staged for commit:
    	modified:   .pnp.cjs
    	modified:   .yarn/install-state.gz
    	modified:   package.json
    	modified:   pages/_app.tsx
    	modified:   pages/index.tsx
    	modified:   yarn.lock
    
    Untracked files:
    	.yarn/cache/react-icons-npm-4.8.0-aa5423cd6c-4dbba7ad98.zip
    	compoents/Footer.tsx
    	compoents/Layout.tsx
    	public/

---
+++ compoents/Navbar.tsx
@@ -0,0 +1,11 @@
+import { RiTeamFill } from "react-icons/ri";
+import { BsVectorPen } from "react-icons/bs";
+import { MdWork } from "react-icons/md";
+import { MdPermPhoneMsg } from "react-icons/md";
+import Image from "next/image";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
