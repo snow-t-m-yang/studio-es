dbf8748329c9d86fd7121c4605aea03889e6f8f3
Young <ytm199891@gmail.com>
2023-04-12 11:31:19 +0800

    Adjust camera and picture's position. add animate.  	modified:   components/Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,18 +1,41 @@
 import Image from "next/image";
+import { useReducer } from "react";
 
 const Contact = () => {
-  return (
-    <section id="聯絡我們" className="grid min-h-screen place-items-center">
-      <div className="px-16 py-3 bg-white shadow-lg rounded-2xl">
-        <h2 className="px-8 font-bold border-b-4 border-primary">聯絡我們</h2>
-      </div>
+  const [isCameraPressed, setIsCameraPressed] = useReducer(
+    (isCameraPressed) => !isCameraPressed,
+    false,
+  );
 
-      <Image
-        src="/contact/contact-camara.svg"
-        width={500}
-        height={500}
-        alt="contact"
-      />
+  return (
+    <section id="聯絡我們" className="min-h-screen">
+      <div className="relative mx-auto w-72">
+        <div className="absolute top-[70px] z-10">
+          <Image
+            src="/contact/contact-camara.svg"
+            width={300}
+            height={300}
+            alt="contact"
+          />
+        </div>
+        <button
+          className=" z-20 absolute w-9 top-[195px] left-[17px] rounded-full h-9 "
+          onClick={() => setIsCameraPressed()}
+        ></button>
+
+        <div
+          className={`${
+            isCameraPressed ? "top-[400px]" : "top-[70px]"
+          } absolute left-6 duration-1000`}
+        >
+          <Image
+            src="/contact/contact-picture.svg"
+            width={240}
+            height={240}
+            alt="contact-picture"
+          />
+        </div>
+      </div>
     </section>
   );
 };
