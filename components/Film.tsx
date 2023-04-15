ea5acfc513f3a6d94d3b28132f3b3ccc149396e5
Young <ytm199891@gmail.com>
2023-04-15 15:11:08 +0800

    Adjust Film position. 	modified:   components/Film.tsx

--- components/Film.tsx
+++ components/Film.tsx
@@ -15,30 +15,30 @@ const Film = () => {
           className="absolute z-20 w-[270px] h-40 left-16 rounded-3xl "
         />
         <div className="absolute w-full h-[220px] -top-[216px] left-[70px] z-10 bg-primary"></div>
-        <div className="absolute z-10 w-ful left-5 h-96">
-          <Image
-            priority
-            src="/film/filmbar.svg"
-            height={300}
-            width={700}
-            alt="A film bar"
-            className=""
-          />
-        </div>
+        <div className="relative w-96 h-96">
+          <div className="absolute z-10 w-ful left-5 h-96">
+            <Image
+              priority
+              src="/film/filmbar.svg"
+              height={300}
+              width={700}
+              alt="A film bar"
+            />
+          </div>
 
-        <div
-          className={`${
-            isFilmBarPressed ? "top-[165px]" : "-top-[210px]"
-          } absolute  left-[73px] z-0 duration-1000`}
-        >
-          <Image
-            priority
-            src="/film/film.svg"
-            height={350}
-            width={250}
-            alt="A film"
-            className=""
-          />
+          <div
+            className={`${
+              isFilmBarPressed ? "top-[160px]" : "-top-[210px]"
+            } absolute  left-[80px] z-0 duration-1000`}
+          >
+            <Image
+              priority
+              src="/film/film.svg"
+              height={330}
+              width={230}
+              alt="A film"
+            />
+          </div>
         </div>
       </div>
     </section>
